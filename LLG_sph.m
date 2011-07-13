f29a7b0b8c55412894e34df467000e200c204da7
David Shepherd <davidshepherd7@gmail.com>
2011-07-13 14:55:10 +0100

    Removed extra copies of files (from moving them wrongly).

--- LLG_sph.m
+++
@@ -1,8 +0,0 @@
-function [dM] = LLG_sph(M, H)
-% The Landau-Lifschitz-Gilbert equation in spherical polar coordinates.
-% M(1) = M_theta, M(2) = M_phi, similarly for H. M_r = |M_s| (fixed)
-
-alpha = 0.8;
-gamma = 0.5;
-
-dM = (gamma/(1 + alpha^2)) * [ -alpha*H(1) - H(2); (1./sin(M(1))) .* (H(1) - alpha*H(2))];
--- normalise.m
+++
@@ -1,5 +0,0 @@
-function [y_out] = normalise(y,Ms)
-
-if nargin <2; Ms = 1; end
-
-y_out = Ms*(y/norm(y,2));
